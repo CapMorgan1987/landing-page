0d3942f5042f45d8153c868ac836468b04e80dbc
CapMorgan1987 <aron.morgan87@gmail.com>
2020-09-30 12:20:08 +0200

    First Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+	<section style="background-color:black">
+		<b-image
+			:src="require('@/assets/img/logo_it_white.webp')"
+			ratio="16by9"
+		></b-image>
+	</section>
+</template>
+
+<script>
+	export default {};
+</script>
