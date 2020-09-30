a058e7e687ecdcba99afb469dea7ad5408b8f4c1
CapMorgan1987 <aron.morgan87@gmail.com>
2020-09-30 22:14:25 +0200

    logo right, padding

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
 	<div id="app">
 		<HelloWorld />
-		<Test />
 
 	</div>
 </template>
 
 <script>
 	import HelloWorld from "./components/HelloWorld.vue";
-	import Test from "./components/Test.vue";
 
 	export default {
 		name: "App",
 		components: {
 			HelloWorld,
-			Test,
 		},
 	};
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,12 @@
 <template>
-	<section style="background-color:black">
-		<b-image
-			style="max-width:200px"
-			:src="require('@/assets/img/logo_it_white.png')"
-		></b-image>
+	<section>
+		<div class="has-background-black py-3 px-3">
+			<b-image
+				style="max-width:200px; margin-left:auto"
+				:src="require('@/assets/img/logo_it_white.webp')"
+			></b-image>
+		</div>
+		<p class="is-size-2 has-text-info">Tekst size 2</p>
 	</section>
 </template>
 
